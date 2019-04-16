f66dbcc5bdbbc37a77bb44a83334373e5c13caa9
uge <ulrich.genner@apgsga.ch>
2019-04-16 18:50:05 +0200

    additional rights for installer

--- packageing/rpm/pre-install.sh
+++ packageing/rpm/pre-install.sh
@@ -8,7 +8,11 @@ if [ "\$1" = "1" ]; then
 	
 	echo "Creating user: ${jenkinsNodeName}"
 	/usr/sbin/useradd -r -c "${jenkinsNodeName} user" ${jenkinsNodeName}  2> /dev/null || :
-
+	echo "Adding Sudoers Rights for ${jenkinsNodeName}"
+	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: $( which yum ) clean all --disablerepo=* --enablerepo=apg-artifactory*" > /etc/sudoers.d/${jenkinsNodeName}
+	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: $( which yum ) -y install --disablerepo=* --enablerepo=apg-artifactory* apg-jadas-service-*" >> /etc/sudoers.d/${jenkinsNodeName}
+	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: !$( which yum ) -y install --disablerepo=* --enablerepo=apg-artifactory* apg-jadas-service-*..*" >> /etc/sudoers.d/${jenkinsNodeName}
+	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: !$( which yum ) -y install --disablerepo=* --enablerepo=apg-artifactory* apg-jadas-service-* *" >> /etc/sudoers.d/${jenkinsNodeName}
 fi
 
 exit 0
