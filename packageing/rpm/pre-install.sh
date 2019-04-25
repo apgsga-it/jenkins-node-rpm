c071ed8f704b0264d854f89de2bc74a845d3f780
uge <ulrich.genner@apgsga.ch>
2019-04-25 21:52:33 +0200

    correct typo

--- packageing/rpm/pre-install.sh
+++ packageing/rpm/pre-install.sh
@@ -9,7 +9,7 @@ if [ "\$1" = "1" ]; then
 	echo "Creating user: \"${jenkinsNodeName}\""
 	/usr/sbin/useradd -r -c "${jenkinsNodeName} user" ${jenkinsNodeName}  2> /dev/null || :
 	echo "Adding Sudoers Rights for \"${jenkinsNodeName}\""
-	echo "Defaults:${jenkinsNodeName} "'!'"norequiretty" > /etc/sudoers.d/${jenkinsNodeName}
+	echo "Defaults:${jenkinsNodeName} "'!'"requiretty" > /etc/sudoers.d/${jenkinsNodeName}
 	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: $( which yum ) clean all --disablerepo=* --enablerepo=apg-artifactory*" >> /etc/sudoers.d/${jenkinsNodeName}
 	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: $( which yum ) -y install --disablerepo=* --enablerepo=apg-artifactory* apg-jadas-service-*" >> /etc/sudoers.d/${jenkinsNodeName}
 	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: "'!'"$( which yum ) -y install --disablerepo=* --enablerepo=apg-artifactory* apg-jadas-service-*..*" >> /etc/sudoers.d/${jenkinsNodeName}
