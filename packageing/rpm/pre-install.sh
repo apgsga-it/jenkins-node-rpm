08a9cb8edf0db4a587e7af5df6fa155da34790eb
uge <ulrich.genner@apgsga.ch>
2019-04-29 21:26:30 +0200

    yum repository options put to variable

--- packageing/rpm/pre-install.sh
+++ packageing/rpm/pre-install.sh
@@ -10,10 +10,11 @@ if [ "\$1" = "1" ]; then
 	/usr/sbin/useradd -r -c "${jenkinsNodeName} user" ${jenkinsNodeName}  2> /dev/null || :
 	echo "Adding Sudoers Rights for \"${jenkinsNodeName}\""
 	echo "Defaults:${jenkinsNodeName} "'!'"requiretty" > /etc/sudoers.d/${jenkinsNodeName}
-	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: $( which yum ) clean all --disablerepo=* --enablerepo=apg-artifactory*" >> /etc/sudoers.d/${jenkinsNodeName}
-	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: $( which yum ) -y install --disablerepo=* --enablerepo=apg-artifactory* apg-jadas-service-*" >> /etc/sudoers.d/${jenkinsNodeName}
-	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: "'!'"$( which yum ) -y install --disablerepo=* --enablerepo=apg-artifactory* apg-jadas-service-*..*" >> /etc/sudoers.d/${jenkinsNodeName}
-	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: "'!'"$( which yum ) -y install --disablerepo=* --enablerepo=apg-artifactory* apg-jadas-service-* *" >> /etc/sudoers.d/${jenkinsNodeName}
+	yumRepoOptions="--disablerepo=* --enablerepo=apg-artifactory*"
+	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: $( which yum ) clean all ${yumRepoOptions}" >> /etc/sudoers.d/${jenkinsNodeName}
+	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: $( which yum ) -y install ${yumRepoOptions} apg-jadas-service-*" >> /etc/sudoers.d/${jenkinsNodeName}
+	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: "'!'"$( which yum ) -y install ${yumRepoOptions} apg-jadas-service-*..*" >> /etc/sudoers.d/${jenkinsNodeName}
+	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: "'!'"$( which yum ) -y install ${yumRepoOptions} apg-jadas-service-* *" >> /etc/sudoers.d/${jenkinsNodeName}
 fi
 
 exit 0
