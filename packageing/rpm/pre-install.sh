d7141c491b1cfa73e677c0ccbc39500e5f0f4649
uge <ulrich.genner@apgsga.ch>
2019-04-30 18:18:03 +0200

    adapt pre-install to gradle-template-syntax and switch to higher next package version (0.6.9)

--- packageing/rpm/pre-install.sh
+++ packageing/rpm/pre-install.sh
@@ -11,10 +11,10 @@ if [ "\$1" = "1" ]; then
 	echo "Adding Sudoers Rights for \"${jenkinsNodeName}\""
 	echo "Defaults:${jenkinsNodeName} "'!'"requiretty" > /etc/sudoers.d/${jenkinsNodeName}
 	yumRepoOptions="--disablerepo=* --enablerepo=apg-artifactory*"
-	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: $( which yum ) clean all ${yumRepoOptions}" >> /etc/sudoers.d/${jenkinsNodeName}
-	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: $( which yum ) -y install ${yumRepoOptions} apg-jadas-service-*" >> /etc/sudoers.d/${jenkinsNodeName}
-	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: "'!'"$( which yum ) -y install ${yumRepoOptions} apg-jadas-service-*..*" >> /etc/sudoers.d/${jenkinsNodeName}
-	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: "'!'"$( which yum ) -y install ${yumRepoOptions} apg-jadas-service-* *" >> /etc/sudoers.d/${jenkinsNodeName}
+	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: \$( which yum ) clean all \${yumRepoOptions}" >> /etc/sudoers.d/${jenkinsNodeName}
+	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: \$( which yum ) -y install \${yumRepoOptions} apg-jadas-service-*" >> /etc/sudoers.d/${jenkinsNodeName}
+	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: "'!'"\$( which yum ) -y install \${yumRepoOptions} apg-jadas-service-*..*" >> /etc/sudoers.d/${jenkinsNodeName}
+	echo "${jenkinsNodeName} ALL= (root) NOPASSWD: "'!'"\$( which yum ) -y install \${yumRepoOptions} apg-jadas-service-* *" >> /etc/sudoers.d/${jenkinsNodeName}
 fi
 
 exit 0
